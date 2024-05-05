586c26e33c0c5b864bc04ae35e881bb4c8c95bfb
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2024-05-05 10:33:37 +0300

    Initial commit

---
+++ backend/src/test/kotlin/com/clockworkcode/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clockworkcode.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
