0fceb7c1dbc20e4483823598e336c16f61caadf5
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2024-05-25 23:34:17 +0200

    Implemented POST request in PaymentBlock

--- frontend/src/Block/PaymentBlock.jsx
+++ frontend/src/Block/PaymentBlock.jsx
@@ -1,12 +1,32 @@
 import {EmbeddedCheckout, EmbeddedCheckoutProvider} from "@stripe/react-stripe-js";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {loadStripe} from "@stripe/stripe-js";
+import axios from "axios";
 
 const stripePubKey = 'pk_test_51KtS1vIBvoqhBs9FLAT83yqmV0oLxIprG0qQuGaLfvKZcu3c9ZVZGATygdBDOkTjLxoFPsq06sqijzLJagg65YJ400GnDhe2av';
 const PaymentBlock = () => {
     const [stripePromise, setStripePromise] = useState(() => loadStripe(stripePubKey))
     const [clientSecret, setClientSecret] = useState("");
     const options = {clientSecret};
+
+    useEffect(() => {
+        // Create a Checkout Session as soon as the page loads
+        axios.post('http://localhost:8080/order/checkout', {
+            licensePlate: JSON.parse(localStorage.getItem('licensePlate'))
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+            }
+        })
+            .then(function (response) {
+                setClientSecret(response.data.clientSecret);
+            })
+            .catch(function (error) {
+                console.log('Error fetching data:', error);
+            })
+    }, []);
 
     return (
         <div>
