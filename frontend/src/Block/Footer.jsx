ebc37df1c88f865557da98d716e904a5a9140ad8
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2024-06-01 21:15:33 +0200

    Updated Footer block
    The year in the Footer block will be automatically updated

--- frontend/src/Block/Footer.jsx
+++ frontend/src/Block/Footer.jsx
@@ -1,9 +1,12 @@
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
+          © {currentYear}{" "}
           <a
             href="https://www.bucharestairports.ro/en"
             className="hover:underline"
